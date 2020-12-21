80e30680428686abd70e2016020cbbab35858f47
AnhDucLe <48914025+ducle6487@users.noreply.github.com>
2020-12-21 19:01:30 +0700

    Add files via upload

---
+++ Sugarless/Sugarless/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Sugarless
+//
+//  Created by Duc'sMacBook on 21/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+    }
+
+
+}
+
