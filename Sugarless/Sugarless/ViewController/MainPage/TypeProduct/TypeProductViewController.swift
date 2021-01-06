34d22728d4f5ba44e7baec01daddc8a0dfb1300b
ducle6487 <ducle6487@gmail.com>
2021-01-06 15:36:16 +0700

    no message

---
+++ Sugarless/Sugarless/ViewController/MainPage/TypeProduct/TypeProductViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TypeProductViewController.swift
+//  Sugarless
+//
+//  Created by Duc'sMacBook on 06/01/2021.
+//
+
+import UIKit
+
+class TypeProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
